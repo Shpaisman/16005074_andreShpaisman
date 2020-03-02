800bd591d45b94bcccb53bd0f9154c691aa60bc1
ANDRE SOIHET SHPAISMAN <16.00507-4@maua.br>
2020-03-02 11:26:21 -0300

    msg

---
+++ Linguagens1_Projetos/Pratica01/aula/meuPrograma.java
@@ -0,0 +1,7 @@
+class meuPrograma {
+    public static void main(String[] args ){
+            System.out.println("Programa java");
+            System.out.println("Modo Conan");
+    }
+    }
+    
--- Linguagens1_Projetos/Pratica01/meuPrograma.java
+++
@@ -1,5 +0,0 @@
-class MeuPrograma {
-    public static void main(string[] args ){
-            System.out.println("Programa java")
-    }
-    }
