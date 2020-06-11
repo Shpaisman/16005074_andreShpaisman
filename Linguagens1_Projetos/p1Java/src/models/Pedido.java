20ebc381e07fa2adf5cf9d84a2f466ed192dff8c
shpaisman <andre.shpaisman@hotmail.com>
2020-06-11 12:33:06 -0300

    id do pedido

--- Linguagens1_Projetos/p1Java/src/models/Pedido.java
+++ Linguagens1_Projetos/p1Java/src/models/Pedido.java
@@ -1,34 +1,36 @@
 package models;
-
 import java.util.Random;
 
-import controller.Orders;
 import enums.FormaPgto;
 import interfaces.Apresentar;
 
-//import enums.Estado;
-//import enums.FormaPgto;
+
 
 
 public class Pedido implements Apresentar{     
     String descricao;
     double valor;
     FormaPgto formaPgto;
-    //Estado estado;
+   
 
     public Pedido(String descricao, double valor, FormaPgto formaPgto){
         this.descricao = descricao;
         this.valor = valor;
         this.formaPgto = formaPgto;
     }
-    
-    public void escolherFormaDePagamento(){
 
+    private String geradorId(){
+        Random random = new Random();
+        String idGerado = "";
+        for(int i=0;i<3;i++)
+            idGerado += random.nextInt(10);
+        return idGerado;
     }
+
 
     @Override
     public void Apresentacao() {
-        System.out.println("id: "  +" Descricao: " + descricao + " Valor: " + valor + " Forma de Pgto: " + formaPgto);
+        System.out.println("id: " + geradorId() +" Descricao: " + descricao + " Valor: " + valor + " Forma de Pgto: " + formaPgto);
     }
 
 }
