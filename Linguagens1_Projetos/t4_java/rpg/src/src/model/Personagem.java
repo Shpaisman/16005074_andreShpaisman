b67f07f5ea2ace407db394a7dd048cfe4867b324
André <andre.shpaisman@hotmail.com>
2020-11-26 09:07:08 -0300

    inicio do projeto

---
+++ Linguagens1_Projetos/t4_java/rpg/src/src/model/Personagem.java
@@ -0,0 +1,22 @@
+package src.model;
+
+/**
+ * Personagem
+ */
+public class Personagem {
+
+    String nome;
+    String raca;
+    String profissao;
+    int mana;
+    int ataque;
+    int ataqueMagico;
+    int defesa;
+    int defesaMagica;
+    int velocidade;
+    int destreza;
+    int exp;
+    int lvl;
+    
+    
+}
