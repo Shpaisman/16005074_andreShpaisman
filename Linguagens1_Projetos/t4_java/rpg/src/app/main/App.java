329a4bc4e3364250e830a3f5df9b4408c9b8a742
André <andre.shpaisman@hotmail.com>
2020-11-26 12:34:21 -0300

    nome dos integrantes

--- Linguagens1_Projetos/t4_java/rpg/src/app/main/App.java
+++ Linguagens1_Projetos/t4_java/rpg/src/app/main/App.java
@@ -1,5 +1,10 @@
 package app.main;
 
+//TRABALHO REALIZADO POR:
+//André Shpaisman 16.00507-4
+//Hector Guerrini 15.01310-3
+
+
 public class App {
     public static void main(String[] args){
         Main main = new Main();
