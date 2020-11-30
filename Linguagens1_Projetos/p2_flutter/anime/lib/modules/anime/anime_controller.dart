62ecfa91c99382db7d4667905e6214118992e935
André <andre.shpaisman@hotmail.com>
2020-11-29 21:19:02 -0300

    trycatch

--- Linguagens1_Projetos/p2_flutter/anime/lib/modules/anime/anime_controller.dart
+++ Linguagens1_Projetos/p2_flutter/anime/lib/modules/anime/anime_controller.dart
@@ -5,13 +5,17 @@ import 'package:anime/modules/anime/anime_model.dart';
 import 'package:http/http.dart' as http;
 
 class AnimeController {
-  String url = 'https://api.jikan.moe/v3/search/anime?q=';
+  static String _url = 'https://api.jikan.moe/v3/search/anime?q=';
 
   Future<Results> fetchAnime(String nome) async {
-    final response = await http.get(url + nome);
+    try {
+      final response = await http.get(_url + nome);
 
-    var animeList = AnimeModel.fromJson(jsonDecode(response.body));
+      var animeList = AnimeModel.fromJson(jsonDecode(response.body));
 
-    return animeList.results.elementAt(0);
+      return animeList.results.elementAt(0);
+    } catch (e) {
+      print(e.toString());
+    }
   }
 }
